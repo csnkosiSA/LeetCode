5b720f0548df1e8d099b6a37af16b6780f1e8677
Sizwe Nkosi <nkscle002@myuct.ac.za>
2022-08-05 23:50:45 +0200

    timing serial algorthms

--- src/MeanFilterSerial.java
+++ src/MeanFilterSerial.java
@@ -8,33 +8,26 @@ import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
-public class MeanFilterSerial{
+public class MeanFilterSerial {
 
-    public static void main(String[] args){
-        
-
-
-
+    public static void main(String[] args) {
 
         /*
-        *the following lines of codes is for 
-        *reading the image
-        */ 
+         * the following lines of codes is for
+         * reading the image
+         */
 
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter <inputImageName> <outputImageName> <windowWidth>");
 
-        String[] data =  input.nextLine().split(" ");
+        String[] data = input.nextLine().split(" ");
 
         String inputImg = data[0];
         String outputImg = data[1];
         int widthq = Integer.parseInt(data[2]);
 
-
-        
-        
-        BufferedImage image  = null;
-        File f  = new File(inputImg);
+        BufferedImage image = null;
+        File f = new File(inputImg);
         try {
 
             image = ImageIO.read(f);
@@ -44,55 +37,54 @@ public class MeanFilterSerial{
             System.out.println("No such image on the directory");
         }
 
-        //to write image 
-        int width =  image.getWidth();
+        // to write image
+        int width = image.getWidth();
         int height = image.getHeight();
 
-        int a  =  0;
+        int a = 0;
         int r = 0;
         int g = 0;
-        int b =0;
-        //f = new File("data/output.jpg");
+        int b = 0;
+        // f = new File("data/output.jpg");
 
         int counter = 0;
         int p = 0;
-        for(int i = widthq-2 ; i<  width-1; i++ ){
-            
-            for(int j = widthq-2; j<height-1; j++){
-                
-               for(int ki = 0; ki<widthq;ki++){
-                   
-                   for(int kj = 0; kj<widthq;kj++){
+        long num1 = System.currentTimeMillis();
+        for (int i = widthq - 2; i < width - 1; i++) {
 
-                        p = image.getRGB(i+ ki -(widthq-2), j+ kj-(widthq-2) );
-                        a =  a + ((p>>24)&0xff);
-                        r = r + ((p>>16)&0xff);
-                        g = g+ ((p>>8)&0xff);     
-                        b = b+ (p&0xff);
-                       // System.out.println(r.get(counter));
+            for (int j = widthq - 2; j < height - 1; j++) {
+
+                for (int ki = 0; ki < widthq; ki++) {
+
+                    for (int kj = 0; kj < widthq; kj++) {
+
+                        p = image.getRGB(i + ki - (widthq - 2), j + kj - (widthq - 2));
+                        a = a + ((p >> 24) & 0xff);
+                        r = r + ((p >> 16) & 0xff);
+                        g = g + ((p >> 8) & 0xff);
+                        b = b + (p & 0xff);
+                        // System.out.println(r.get(counter));
                         counter++;
-                   }  
-                        
-               }
-              
-               
-               
-               int dar  = ((widthq*widthq)/2);
+                    }
 
-               p = (a<<24) | (r/counter<<16) | (g/counter<<8) | b/counter;
-               image.setRGB(i-(widthq-2), j-(widthq-2), p);
+                }
 
-              counter =0;
-              a = 0;
-              r=0;
-              g=0;
-              b=0;
-             
-               
+                int dar = ((widthq * widthq) / 2);
+
+                p = (a << 24) | (r / counter << 16) | (g / counter << 8) | b / counter;
+                image.setRGB(i - (widthq - 2), j - (widthq - 2), p);
+
+                counter = 0;
+                a = 0;
+                r = 0;
+                g = 0;
+                b = 0;
+
             }
 
         }
-        //get pixel value
+        long num2 = System.currentTimeMillis();
+        // get pixel value
 
         f = new File(outputImg);
         try {
@@ -102,8 +94,8 @@ public class MeanFilterSerial{
             e.printStackTrace();
         }
 
+        System.out.println("Took about " + (num2 - num1) / (float) 1000 + " seconds");
         System.out.println("Done");
     }
 
-    
 }
--- src/MedianFilterSerial.java
+++ src/MedianFilterSerial.java
@@ -2,39 +2,31 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
-public class MedianFilterSerial{
+public class MedianFilterSerial {
 
-    public static void main(String[] args){
-        
-
-
-
+    public static void main(String[] args) {
 
         /*
-        *the following lines of codes is for 
-        *reading the image
-        */ 
+         * the following lines of codes is for
+         * reading the image
+         */
 
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter <inputImageName> <outputImageName> <windowWidth>");
 
-        String[] data =  input.nextLine().split(" ");
+        String[] data = input.nextLine().split(" ");
 
         String inputImg = data[0];
         String outputImg = data[1];
         int widthq = Integer.parseInt(data[2]);
 
-
-        
-        
-        BufferedImage image  = null;
-        File f  = new File(inputImg);
+        BufferedImage image = null;
+        File f = new File(inputImg);
         try {
 
             image = ImageIO.read(f);
@@ -44,56 +36,57 @@ public class MedianFilterSerial{
             System.out.println("No such image on the directory");
         }
 
-        //to write image 
-        int width =  image.getWidth();
+        // to write image
+        int width = image.getWidth();
         int height = image.getHeight();
 
-        ArrayList<Integer> a  =  new ArrayList<Integer>();
-        ArrayList<Integer> r  =  new ArrayList<Integer>();
-        ArrayList<Integer> g  =  new ArrayList<Integer>();
-        ArrayList<Integer> b  =  new ArrayList<Integer>();
-        //f = new File("data/output.jpg");
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        ArrayList<Integer> r = new ArrayList<Integer>();
+        ArrayList<Integer> g = new ArrayList<Integer>();
+        ArrayList<Integer> b = new ArrayList<Integer>();
+        // f = new File("data/output.jpg");
 
         int counter = 0;
         int p = 0;
-        for(int i = widthq-2 ; i<  width-1; i++ ){
-            
-            for(int j = widthq-2; j<height-1; j++){
+        long num1 = System.currentTimeMillis();
+        for (int i = widthq - 2; i < width - 1; i++) {
 
-               for(int ki = 0; ki<widthq;ki++){
-                   
-                   for(int kj = 0; kj<widthq;kj++){
+            for (int j = widthq - 2; j < height - 1; j++) {
 
-                        p = image.getRGB(i+ ki -(widthq-2), j+ kj-(widthq-2) );
-                        a.add((p>>24)&0xff);
-                        r.add((p>>16)&0xff);
-                        g.add((p>>8)&0xff);     
-                        b.add(p&0xff);
-                       // System.out.println(r.get(counter));
+                for (int ki = 0; ki < widthq; ki++) {
+
+                    for (int kj = 0; kj < widthq; kj++) {
+
+                        p = image.getRGB(i + ki - (widthq - 2), j + kj - (widthq - 2));
+                        a.add((p >> 24) & 0xff);
+                        r.add((p >> 16) & 0xff);
+                        g.add((p >> 8) & 0xff);
+                        b.add(p & 0xff);
+                        // System.out.println(r.get(counter));
                         counter++;
-                   }  
-                        
-               }
-               counter = 0;
-               
-               Collections.sort(r);
-               Collections.sort(g);
-               Collections.sort(b);
-               int dar  = ((widthq*widthq)/2);
+                    }
 
-               p = (a.get(dar)<<24) | (r.get(dar)<<16) | (g.get(dar)<<8) | b.get(dar);
-               image.setRGB(i-(widthq-2), j-(widthq-2), p);
+                }
+                counter = 0;
 
-               a.clear();
-               r.clear();
-               g.clear();
-               b.clear();
-             
-               
+                Collections.sort(r);
+                Collections.sort(g);
+                Collections.sort(b);
+                int dar = ((widthq * widthq) / 2);
+
+                p = (a.get(dar) << 24) | (r.get(dar) << 16) | (g.get(dar) << 8) | b.get(dar);
+                image.setRGB(i - (widthq - 2), j - (widthq - 2), p);
+
+                a.clear();
+                r.clear();
+                g.clear();
+                b.clear();
+
             }
 
         }
-        //get pixel value
+        long num2 = System.currentTimeMillis();
+        // get pixel value
 
         f = new File(outputImg);
         try {
@@ -103,8 +96,8 @@ public class MedianFilterSerial{
             e.printStackTrace();
         }
 
-        System.out.println("Done");
+        System.out.println("Took about " + (num2 - num1) / (float) 1000 + " seconds");
+
     }
 
-    
 }
